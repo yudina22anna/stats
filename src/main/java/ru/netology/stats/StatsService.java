6e5f5a8a55ebb6d23d3acc8878eb492f3e438f8c
annayudina <yudina.anna.zi15@gmail.com>
2022-09-26 09:54:19 +0300

    add

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -1,2 +1,68 @@
-package ru.netology.stats;public class StatsService {
+package ru.netology.stats;
+
+public class StatsService {
+    public long calculateSum(int[] values) {
+        int month = 0;
+        for (int value : values) {
+            month += value;
+        }
+        return month;
+    }
+
+    public long calculateAverageSum(int[] values) {
+        int averageMonth = 0;
+        for (int value : values) {
+            averageMonth += value;
+        }
+        averageMonth = averageMonth / 12;
+        return averageMonth;
+    }
+
+    public long calculateMaxMonth(int[] values) {
+        int monthMax = values[0];
+        for (int value : values) {
+            if (monthMax < value) {
+                monthMax = value;
+            }
+        }
+        return monthMax;
+    }
+
+    public long calculateMinMonth(int[] values) {
+        int monthMin = values[0];
+        for (int value : values) {
+            if (monthMin > value) {
+                monthMin = value;
+            }
+        }
+        return monthMin;
+    }
+
+    public long calculateBelowAverage(int[] values) {
+        int averageMonth = 0;
+        for (int value : values) {
+            averageMonth += value;
+        }
+        averageMonth = averageMonth / 12;
+        int belowAverageMonth = 0;
+        for (int value : values) {
+            if (value < averageMonth) belowAverageMonth++;
+        }
+        return belowAverageMonth;
+    }
+
+    public long calculateAboveAverage(int[] values) {
+        int averageMonth = 0;
+        for (int value : values) {
+            averageMonth += value;
+        }
+        averageMonth = averageMonth / 12;
+        int aboveAverageMonth = 0;
+        for (int value : values) {
+            if (value >= averageMonth) aboveAverageMonth++;
+        }
+        return aboveAverageMonth;
+    }
 }
+
+
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -1,2 +1,63 @@
-package ru.netology.stats;public class StatsServiceTest {
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StatsServiceTest {
+
+    @Test
+    void shouldCalculateSum() {
+        StatsService service = new StatsService();
+        int values[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 180;
+        long actual = service.calculateSum(values);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateAverageSum() {
+        StatsService service = new StatsService();
+        int values[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 15;
+        long actual = service.calculateAverageSum(values);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateMaxMonth() {
+        StatsService service = new StatsService();
+        int values[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 20;
+        long actual = service.calculateMaxMonth(values);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateMinMonth() {
+        StatsService service = new StatsService();
+        int values[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 7;
+        long actual = service.calculateMinMonth(values);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateBelowAverage() {
+        StatsService service = new StatsService();
+        int values[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 5;
+        long actual = service.calculateBelowAverage(values);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateAboveAverage() {
+        StatsService service = new StatsService();
+        int values[] = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 7;
+        long actual = service.calculateAboveAverage(values);
+        assertEquals(expected, actual);
+    }
+
 }
